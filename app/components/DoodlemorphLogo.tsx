611f279ddcd194a39de391942072b3531799d1bc
kingstarfly <xingxiang@twotreesgroup.com>
2025-10-18 21:10:47 +0800

    add logo to canvas

--- app/components/DoodlemorphLogo.tsx
+++ app/components/DoodlemorphLogo.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image'
+
 export function DoodlemorphLogo() {
-	return <div className="doodlemorph-logo">DoodleMorph</div>
+	return (
+		<div className="doodlemorph-logo">
+			<Image
+				src="/doodlemorph-logo.png"
+				alt="DoodleMorph Logo"
+				width={20}
+				height={20}
+				priority
+				className="doodlemorph-logo-image"
+			/>
+			<span className="doodlemorph-text">DoodleMorph</span>
+		</div>
+	)
 }
